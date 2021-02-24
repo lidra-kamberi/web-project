e606d156664b30998718aa11b770750ffb96d03c
lidra-kamberi <lk47525@ubt-uni.net>
2021-02-24 17:40:12 +0100

    created the database file and the corresponding tables

---
+++ h-skincare.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 24, 2021 at 05:34 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `h-skincare`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aboutus`
+--
+
+CREATE TABLE `aboutus` (
+  `ID` int(11) NOT NULL,
+  `main_title` varchar(50) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `aboutus`
+--
+
+INSERT INTO `aboutus` (`ID`, `main_title`, `title`, `content`) VALUES
+(1, 'H-SKINCARE', 'ABOUT US', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `ID` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`ID`, `name`, `email`, `comment`) VALUES
+(11, 'Erdita Jaha', 'erditajaha@gmail.com', 'Amazing skincare line! Recommend it!'),
+(12, 'Loreta Pajaziti', 'loretapajaziti@gmail.com', 'My skin has never felt better! Highly recommend!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home-slider`
+--
+
+CREATE TABLE `home-slider` (
+  `ID` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ID` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL,
+  `created_date` date NOT NULL,
+  `created_by` varchar(50) DEFAULT NULL,
+  `edited_by` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ID`, `title`, `image`, `price`, `created_date`, `created_by`, `edited_by`) VALUES
+(10, 'LAVENDER', 'images/product.jpg', 15, '2021-02-09', '', 'flutura.hundozi'),
+(11, 'ROSEMARY', 'images/product-2.jpg', 25, '2021-02-02', '', 'flutura.hundozi'),
+(12, 'PEPPERMINT', 'images/product-3.jpg', 21, '2021-02-11', '', 'lidra.kamberi'),
+(13, 'SANDALWOOD', 'images/product-3.jpg', 23, '2021-02-10', '', 'lidra.kamberi'),
+(14, 'BERGAMOT ESSENTIAL OIL', 'images/product.jpg', 19, '2021-01-31', '', ''),
+(15, 'JASMINE ESSENTIAL OIL', 'images/product-2.jpg', 14, '2021-02-13', '', ''),
+(18, 'ROSE HIP OIL', 'images/product-3.jpg', 20, '2021-02-14', 'flutura.hundozi', 'lidra.kamberi'),
+(19, 'CHAMOMILE', 'images/product-2.jpg', 30, '2021-02-10', 'lidra.kamberi', 'flutura.hundozi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `psw` varchar(50) NOT NULL,
+  `roli` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID`, `email`, `username`, `psw`, `roli`) VALUES
+(1, 'lk47525@ubt-uni.net', 'lidra.kamberi', 'Lidra123!', 1),
+(2, 'fh47422@ubt-uni.net', 'flutura.hundozi', 'Flutura123!', 1),
+(3, 'asd123@ubt-uni.net', 'qwerty', 'Qwerty123!', 0),
+(4, 'test123@ubt-uni.net', 'testtest', 'Test123!', 0),
+(5, 'test@test.com', 'tetsing', 'test123!A', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aboutus`
+--
+ALTER TABLE `aboutus`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `home-slider`
+--
+ALTER TABLE `home-slider`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aboutus`
+--
+ALTER TABLE `aboutus`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `home-slider`
+--
+ALTER TABLE `home-slider`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
